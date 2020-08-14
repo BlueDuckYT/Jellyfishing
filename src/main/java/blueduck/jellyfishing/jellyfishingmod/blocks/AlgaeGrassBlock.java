a77834dc4b90bd7a330667545fbe9e68288037ae
Max <bludux595@gmail.com>
2020-08-14 13:11:13 -0400

    Bugs
    Signed-off-by: Max <bludux595@gmail.com>

--- src/main/java/blueduck/jellyfishing/jellyfishingmod/blocks/AlgaeGrassBlock.java
+++ src/main/java/blueduck/jellyfishing/jellyfishingmod/blocks/AlgaeGrassBlock.java
@@ -1,12 +1,20 @@
 package blueduck.jellyfishing.jellyfishingmod.blocks;
 
 import blueduck.jellyfishing.jellyfishingmod.registry.JellyfishingBlocks;
+import blueduck.jellyfishing.jellyfishingmod.registry.JellyfishingFeatures;
+import com.google.common.collect.Lists;
 import net.minecraft.block.*;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IBlockReader;
 import net.minecraft.world.World;
+import net.minecraft.world.gen.feature.ConfiguredFeature;
+import net.minecraft.world.gen.feature.DecoratedFeatureConfig;
+import net.minecraft.world.gen.feature.FlowersFeature;
+import net.minecraft.world.gen.placement.CountConfig;
 import net.minecraft.world.server.ServerWorld;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 public class AlgaeGrassBlock extends Block {
@@ -30,7 +38,4 @@ public class AlgaeGrassBlock extends Block {
             worldIn.setBlockState(pos, state);
         }
     }
-
-
-
 }
