c73a794e5b00538a4bbe5b020cc2d2a2433e4f9b
Max <bludux595@gmail.com>
2020-11-24 18:45:35 -0500

    Actually fixed
    Signed-off-by: Max <bludux595@gmail.com>

--- src/main/java/blueduck/jellyfishing/jellyfishingmod/registry/JellyfishingEntities.java
+++ src/main/java/blueduck/jellyfishing/jellyfishingmod/registry/JellyfishingEntities.java
@@ -17,8 +17,8 @@ public class JellyfishingEntities {
     public static final DeferredRegister<EntityType<?>> ENTITIES = DeferredRegister.create(ForgeRegistries.ENTITIES, JellyfishingMod.MODID);
 
 
-    public static final RegistryObject<EntityType<JellyfishEntity>> JELLYFISH = ENTITIES.register("jellyfish", () -> EntityType.Builder.<JellyfishEntity>create(JellyfishEntity::new, EntityClassification.WATER_CREATURE).size(0.5F, 0.4F).build(new ResourceLocation("jellyfishing", "textures/entities/jellyfish.png").toString()));
-    public static final RegistryObject<EntityType<BlueJellyfishEntity>> BLUE_JELLYFISH = ENTITIES.register("blue_jellyfish", () -> EntityType.Builder.<BlueJellyfishEntity>create(BlueJellyfishEntity::new, EntityClassification.WATER_CREATURE).size(0.5F, 0.4F).build(new ResourceLocation("jellyfishing", "textures/entities/blue_jellyfish.png").toString()));
+    public static final RegistryObject<EntityType<JellyfishEntity>> JELLYFISH = ENTITIES.register("jellyfish", () -> EntityType.Builder.<JellyfishEntity>create(JellyfishEntity::new, EntityClassification.WATER_AMBIENT).size(0.5F, 0.4F).build(new ResourceLocation("jellyfishing", "textures/entities/jellyfish.png").toString()));
+    public static final RegistryObject<EntityType<BlueJellyfishEntity>> BLUE_JELLYFISH = ENTITIES.register("blue_jellyfish", () -> EntityType.Builder.<BlueJellyfishEntity>create(BlueJellyfishEntity::new, EntityClassification.WATER_AMBIENT).size(0.5F, 0.4F).build(new ResourceLocation("jellyfishing", "textures/entities/blue_jellyfish.png").toString()));
 
     public static final RegistryObject<EntityType<MinecartEntity>> PATTY_WAGON = ENTITIES.register("patty_wagon", () -> EntityType.Builder.<MinecartEntity>create(MinecartEntity::new, EntityClassification.MISC).size(0.98F, 0.7F).build(new ResourceLocation("jellyfishing", "textures/entities/patty_wagon").toString()));
 
